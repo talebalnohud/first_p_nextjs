6b385d8ae591c64fe1cb0d6777ddd24313311d76
unknown <taleb.alnohoud@gmail.com>
2024-10-16 14:02:55 +0300

    authentication session

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
   // console.log(process.env.GOOGLE_CLIENT_SECRET!);
+  const { status, data: session } = useSession();
+
   return (
     <div className="flex bg-sky-600 text-white font-bold py-5 space-x-3">
       <Link href="/" className="mx-5">
@@ -11,7 +15,16 @@ const NavBar = () => {
       <Link href="/users">Users</Link>
       <Link href="/admin">Admin</Link>
       <Link href="/product">Product</Link>
-      <Link href="/api/auth/signin">Login</Link>
+      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "authenticated" && (
+        <img
+          src={session.user!.image}
+          className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+        />
+      )}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+"use strict";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <main className="flex justify-center items-center flex-col">
-      <h1>Hello World!</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="users" className="btn btn-primary px-5 mb-3">
         Users
       </Link>
