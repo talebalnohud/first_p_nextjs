594d28b380151cc88f4666236857f4932f31d512
unknown <taleb.alnohoud@gmail.com>
2024-10-21 22:00:09 +0300

    try to fix

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,10 +1,26 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { CldUploadWidget } from "next-cloudinary";
 
+interface CloudinaryResult {
+  public_id: string;
+}
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset="y8kfb9pc">
+    <CldUploadWidget
+      uploadPreset="y8kfb9pc"
+      options={{
+        sources: ["local"],
+        multiple: false,
+        maxFiles: 5,
+      }}
+      onUpload={(result, widget) => {
+        if (result.event !== "success") return;
+        const info = result.info as CloudinaryResult;
+        setPublicId(info.public_id);
+      }}
+    >
       {({ open }) => (
         <button className="btn" onClick={() => open()}>
           Upload
@@ -15,3 +31,6 @@ const UploadPage = () => {
 };
 
 export default UploadPage;
+
+
+
